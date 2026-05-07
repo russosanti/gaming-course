3efcb8b6dbc3bf8e4387c1110a1904765a7410fc
Santiago Russo <russosanti@hotmail.com>
2026-05-07 01:17:36 -0300

    birds

---
+++ flappy-birds/bird-4/main.lua
@@ -0,0 +1,135 @@
+--[[
+    CS50 2D
+    Flappy Bird Remake
+
+    bird-4
+    "The Anti-Gravity Update"
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple
+    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping
+    the screen, making the player's bird avatar flap its wings and move upwards slightly.
+    A variant of popular games like "Helicopter Game" that floated around the internet
+    for years prior. Illustrates some of the most basic procedural generation of game
+    levels possible as by having pipes stick out of the ground by varying amounts, acting
+    as an infinitely generated obstacle course for the player.
+]]
+
+-- virtual resolution handling library
+push = require 'push'
+
+-- classic OOP class library
+Class = require 'class'
+
+-- bird class we've written
+require 'Bird'
+
+-- physical screen dimensions
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- background starting scroll location (X axis)
+local backgroundScroll = 0
+
+-- ground starting scroll location (X axis)
+local groundScroll = 0
+
+-- speed at which we should scroll our images, scaled by dt
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which we should loop our background back to X 0
+local BACKGROUND_LOOPING_POINT = 413
+
+function love.load()
+    -- initialize our nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- images we load into memory from files to later draw onto the screen
+    background = love.graphics.newImage('background.png')
+    ground = love.graphics.newImage('ground.png')
+
+    -- our bird sprite
+    bird = Bird()
+
+    -- app window title
+    love.window.setTitle('Fifty Bird')
+
+    -- initialize window
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    -- initialize our virtual resolution
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+
+    -- initialize input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push.resize(w, h)
+end
+
+function love.keypressed(key)
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
+    New function used to check our global input table for keys we activated during
+    this frame, looked up by their string value.
+]]
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    -- scroll background by preset speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+
+    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
+     if love.keyboard.isDown('space') then
+        love.keyboard.keysPressed['space'] = true
+     end
+
+    bird:update(dt)
+
+    -- reset input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push.start()
+
+    -- draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    -- draw the ground on top of the background, toward the bottom of the screen,
+    -- at its negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    -- render our bird to the screen using its own render logic
+    bird:render()
+
+    push.finish()
+end
